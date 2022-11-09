9edefb0d283b731e45a2d145251057e7bf5d8e5e
Shubham Kharade <shubhamkharade4395@gmail.com>
2022-11-09 15:07:23 +0530

    Javascript Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>
+        Basic Example of JavaScript
+    </title>
+</head>
+ 
+<body>
+    <script>
+        const add = (x+y)=> x+y;
+        const sub = (x-y) => x-y;
+        console.log(add(5,6));
+        console.log(sub(10,5));
+    </script>
+</body>
+</html>
